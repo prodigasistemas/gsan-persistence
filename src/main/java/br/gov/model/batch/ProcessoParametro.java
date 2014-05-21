878ffde53d46d5377c0a356b79682d756825d925
Paulo Igor <piagodinho@gmail.com>
2014-05-21 10:28:10 -0300

    Adicionando mapeamento da tabela processo parametros

---
+++ src/main/java/br/gov/model/batch/ProcessoParametro.java
@@ -0,0 +1,73 @@
+package br.gov.model.batch;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="processo",schema="batch")
+public class ProcessoParametro implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4319013335858219463L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_PROCESSO_PARAMETROS")
+	@SequenceGenerator(name="SEQ_PROCESSO_PARAMETROS", sequenceName="seq_processo_parametros")
+	@Column(name="prpr_id")
+	private Integer id;
+	
+	@ManyToOne
+	@JoinColumn(name="proi_id")
+	private ProcessoIniciado processoIniciado;
+	
+	@Column(name="prpr_nmparametro")
+	private String nomeParametro;
+	
+	@Column(name="prpr_valorparametro")
+	private String valor;
+	
+	public ProcessoParametro(){}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public ProcessoIniciado getProcessoIniciado() {
+		return processoIniciado;
+	}
+
+	public void setProcessoIniciado(ProcessoIniciado processoIniciado) {
+		this.processoIniciado = processoIniciado;
+	}
+
+	public String getNomeParametro() {
+		return nomeParametro;
+	}
+
+	public void setNomeParametro(String nomeParametro) {
+		this.nomeParametro = nomeParametro;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+}
