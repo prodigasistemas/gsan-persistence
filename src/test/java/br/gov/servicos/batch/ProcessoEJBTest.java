d13f0862c351c9b0b2d03c76a46a1b42edf7f22f
Paulo Igor <piagodinho@gmail.com>
2014-05-29 16:24:21 -0300

    Adicionando framework e teste de exemplo para o desenvolvimento da suite de testes do projeto

---
+++ src/test/java/br/gov/servicos/batch/ProcessoEJBTest.java
@@ -0,0 +1,43 @@
+package br.gov.servicos.batch;
+
+import static junit.framework.Assert.assertNotNull;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.gov.model.batch.ProcessoIniciado;
+import br.gov.model.batch.ProcessoSituacao;
+
+
+@RunWith(Arquillian.class)
+public class ProcessoEJBTest {
+	
+	@Deployment
+    public static Archive<?> createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "test.jar")
+            .addPackage(ProcessoEJB.class.getPackage())
+            .addPackage(ProcessoIniciado.class.getPackage())
+            .addAsResource("META-INF/persistence.xml")
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+	
+	@Inject
+	ProcessoEJB processoEJB;
+	
+	@Test
+	public void buscarProcessosPorSituacao() throws Exception {
+		List<ProcessoIniciado> processosIniciados = processoEJB.buscarProcessosPorSituacao(ProcessoSituacao.EM_ESPERA);
+		
+		assertNotNull(processosIniciados);
+	}
+}
