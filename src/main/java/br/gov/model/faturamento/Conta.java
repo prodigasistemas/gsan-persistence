aec222657f895e802e938457a947f0d9a5d67f52
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2014-07-10 11:18:40 -0300

    retirando import desnecessario

--- src/main/java/br/gov/model/faturamento/Conta.java
+++ src/main/java/br/gov/model/faturamento/Conta.java
@@ -9,7 +9,6 @@ import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.Date;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
