3f77187cf4c47ec9d5146e739eb3e31bc150bb48
Paulo Igor <piagodinho@gmail.com>
2014-08-26 15:08:08 -0300

    Adicionando novos campos e relacionamentos na entidade

--- src/main/java/br/gov/model/arrecadacao/DebitoAutomaticoMovimento.java
+++ src/main/java/br/gov/model/arrecadacao/DebitoAutomaticoMovimento.java
@@ -1,6 +1,8 @@
 package br.gov.model.arrecadacao;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,6 +15,7 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import br.gov.model.faturamento.ContaGeral;
+import br.gov.model.faturamento.FaturamentoGrupo;
 
 @Entity
 @Table(schema="arrecadacao", name="debito_auto_movimento")
@@ -28,6 +31,38 @@ public class DebitoAutomaticoMovimento implements Serializable{
 	@ManyToOne
 	@JoinColumn(name="cnta_id")
 	private ContaGeral contaGeral;
+	
+	@Column(name="damv_dtvencimento")
+	private Date dataVencimento;
+	
+	@Column(name="damv_vldebito")
+	private BigDecimal valorDebito;
+	
+	@Column(name="damv_tmprocessamento")
+	private Date processamento;
+	
+	@Column(name="damv_tmenviobanco")
+	private Date envioBanco;
+	
+	@Column(name="damv_tmretornobanco")
+	private Date retornoBanco;
+	
+	@Column(name="damv_tmultimaalteracao")
+	private Date ultimaAlteracao;
+	
+	@Column(name="damv_nnnsaenvio")
+	private Integer numeroSequenciaArquivoEnviado;
+	
+	@Column(name="damv_nnnsaretorno")
+	private Integer numeroSequenciaArquivoRecebido;
+	
+	@ManyToOne
+	@JoinColumn(name="ftgr_id")
+	private FaturamentoGrupo faturamentoGrupo;
+	
+	@ManyToOne
+	@JoinColumn(name="deba_id")
+	private DebitoAutomatico debitoAutomatico;
 	
 	public DebitoAutomaticoMovimento() {
 	}
@@ -46,6 +81,86 @@ public class DebitoAutomaticoMovimento implements Serializable{
 
 	public void setContaGeral(ContaGeral contaGeral) {
 		this.contaGeral = contaGeral;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public BigDecimal getValorDebito() {
+		return valorDebito;
+	}
+
+	public void setValorDebito(BigDecimal valorDebito) {
+		this.valorDebito = valorDebito;
+	}
+
+	public Date getProcessamento() {
+		return processamento;
+	}
+
+	public void setProcessamento(Date processamento) {
+		this.processamento = processamento;
+	}
+
+	public Date getEnvioBanco() {
+		return envioBanco;
+	}
+
+	public void setEnvioBanco(Date envioBanco) {
+		this.envioBanco = envioBanco;
+	}
+
+	public Date getRetornoBanco() {
+		return retornoBanco;
+	}
+
+	public void setRetornoBanco(Date retornoBanco) {
+		this.retornoBanco = retornoBanco;
+	}
+
+	public Date getUltimaAlteracao() {
+		return ultimaAlteracao;
+	}
+
+	public void setUltimaAlteracao(Date ultimaAlteracao) {
+		this.ultimaAlteracao = ultimaAlteracao;
+	}
+
+	public Integer getNumeroSequenciaArquivoEnviado() {
+		return numeroSequenciaArquivoEnviado;
+	}
+
+	public void setNumeroSequenciaArquivoEnviado(Integer numeroSequenciaArquivoEnviado) {
+		this.numeroSequenciaArquivoEnviado = numeroSequenciaArquivoEnviado;
+	}
+
+	public Integer getNumeroSequenciaArquivoRecebido() {
+		return numeroSequenciaArquivoRecebido;
+	}
+
+	public void setNumeroSequenciaArquivoRecebido(Integer numeroSequenciaArquivoRecebido) {
+		this.numeroSequenciaArquivoRecebido = numeroSequenciaArquivoRecebido;
+	}
+	
+	public FaturamentoGrupo getFaturamentoGrupo() {
+		return faturamentoGrupo;
+	}
+
+	public void setFaturamentoGrupo(FaturamentoGrupo faturamentoGrupo) {
+		this.faturamentoGrupo = faturamentoGrupo;
+	}
+	
+	public DebitoAutomatico getDebitoAutomatico() {
+		return debitoAutomatico;
+	}
+
+	public void setDebitoAutomatico(DebitoAutomatico debitoAutomatico) {
+		this.debitoAutomatico = debitoAutomatico;
 	}
 
 	public String toString() {
