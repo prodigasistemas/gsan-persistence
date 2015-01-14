f1885cd022ef7eb2cf091cac6be274ac9a346920
Felipe Santos <felipesantos2089@gmail.com>
2015-01-14 11:44:32 -0300

    Adicionando HidrometroMedicaoHistoricoTO para a geração do ArquivoTextoTipo08

--- src/main/java/br/gov/servicos/micromedicao/MedicaoHistoricoRepositorio.java
+++ src/main/java/br/gov/servicos/micromedicao/MedicaoHistoricoRepositorio.java
@@ -13,50 +13,47 @@ import br.gov.model.micromedicao.MedicaoHistorico;
 
 @Stateless
 public class MedicaoHistoricoRepositorio {
-	
+
 	@PersistenceContext
 	private EntityManager entity;
 
 	public MedicaoHistorico buscarPorImovelEReferencia(Integer idImovel, Integer anoMesReferencia) {
 		StringBuilder sql = new StringBuilder();
 		sql.append("select mdhi from MedicaoHistorico mdhi")
-		.append(" INNER JOIN mdhi.ligacaoAgua lagu ")
-		.append(" WHERE lagu.imovel.id = :idImovel ")
-		.append(" AND mdhi.anoMesReferencia = :anoMesReferencia ");
-		
-		
+		   .append(" INNER JOIN mdhi.ligacaoAgua lagu ")
+		   .append(" WHERE lagu.imovel.id = :idImovel ")
+		   .append(" AND mdhi.anoMesReferencia = :anoMesReferencia ");
+
 		List<MedicaoHistorico> lista = entity.createQuery(sql.toString(), MedicaoHistorico.class)
 				.setParameter("idImovel", idImovel)
 				.setParameter("anoMesReferencia", anoMesReferencia)
-				.setMaxResults(1)
-				.getResultList();
-		
+				.setMaxResults(1).getResultList();
+
 		return lista.size() > 0 ? lista.get(0) : null;
 	}
-	
-	public MedicaoHistorico buscarPorLigacaoAguaOuPoco(Integer idImovel, Integer anoMesReferencia){
+
+	public MedicaoHistorico buscarPorLigacaoAguaOuPoco(Integer idImovel, Integer anoMesReferencia) {
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT mdhi ")
-			.append(" FROM MedicaoHistorico mdhi ")
-			.append(" LEFT JOIN mdhi.ligacaoAgua lagu ")
-			.append(" LEFT JOIN mdhi.imovel imovel ")
-			.append(" WHERE mdhi.anoMesReferencia = :anoMesReferencia ")
-			.append(" AND (imovel.id = :idImovel OR lagu.imovel.id = :idImovel) ");
-			
+		   .append(" FROM MedicaoHistorico mdhi ")
+		   .append(" LEFT JOIN mdhi.ligacaoAgua lagu ")
+		   .append(" LEFT JOIN mdhi.imovel imovel ")
+		   .append(" LEFT JOIN mdhi.leituraSituacaoAtual leituraSituacaoAtual ")
+		   .append(" WHERE mdhi.anoMesReferencia = :anoMesReferencia ")
+		   .append(" AND (imovel.id = :idImovel OR lagu.imovel.id = :idImovel) ");
+
 		try {
 			return entity.createQuery(sql.toString(), MedicaoHistorico.class)
-						.setParameter("anoMesReferencia", anoMesReferencia)
-						.setParameter("idImovel", idImovel)
-						.setMaxResults(1)
-						.getSingleResult();
+					.setParameter("anoMesReferencia", anoMesReferencia)
+					.setParameter("idImovel", idImovel)
+					.setMaxResults(1).getSingleResult();
 		} catch (NoResultException e) {
 			return null;
 		}
 	}
-	
+
 	public Integer buscarLeituraAnormalidadeFaturamento(ConsumoHistorico consumoHistorico) {
 		StringBuilder sql = new StringBuilder();
-
 		sql.append("SELECT leituraAnormalidadeFaturamento.id ");
 		sql.append("FROM MedicaoHistorico medicaoHistorico ");
 		sql.append("INNER JOIN medicaoHistorico.leituraAnormalidadeFaturamento leituraAnormalidadeFaturamento ");
@@ -71,7 +68,7 @@ public class MedicaoHistoricoRepositorio {
 
 		Integer retorno = entity.createQuery(sql.toString(), Integer.class)
 				.setParameter("idImovel", consumoHistorico.getImovel().getId())
-				.setParameter("anoMesReferencia",consumoHistorico.getReferenciaFaturamento())
+				.setParameter("anoMesReferencia", consumoHistorico.getReferenciaFaturamento())
 				.setMaxResults(1).getSingleResult();
 
 		return retorno;
