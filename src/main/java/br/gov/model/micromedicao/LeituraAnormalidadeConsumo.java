77cffcc16f06250ff332ed56de9ce6a2d365909f
Pamela Gatinho <pamelagatinho@gmail.com>
2015-01-21 15:42:20 -0300

    Gerar dados de situação especial de faturamento - arquivo tipo 1

--- src/main/java/br/gov/model/micromedicao/LeituraAnormalidadeConsumo.java
+++ src/main/java/br/gov/model/micromedicao/LeituraAnormalidadeConsumo.java
@@ -1,5 +1,7 @@
 package br.gov.model.micromedicao;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -13,6 +15,21 @@ public class LeituraAnormalidadeConsumo {
 	@Column(name="lacs_id")
 	private Integer id;
 	
+	@Column(name="lacs_dsconsumoacobrar")
+	private String descricaoConsumoACobrar;
+	
+	@Column(name="lacs_icsemleitura")
+	private Short semLeitura;
+	
+	@Column(name="lacs_iccomleitura")
+	private Short comLeitura;
+	
+	@Column(name="lacs_icuso")
+	private Short indicadorUso;
+	
+	@Column(name="lacs_tmultimaalteracao")	
+	private Date ultimaAlteracao;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -21,4 +38,43 @@ public class LeituraAnormalidadeConsumo {
 		this.id = id;
 	}
 
+	public String getDescricaoConsumoACobrar() {
+		return descricaoConsumoACobrar;
+	}
+
+	public void setDescricaoConsumoACobrar(String descricaoConsumoACobrar) {
+		this.descricaoConsumoACobrar = descricaoConsumoACobrar;
+	}
+
+	public Short getSemLeitura() {
+		return semLeitura;
+	}
+
+	public void setSemLeitura(Short semLeitura) {
+		this.semLeitura = semLeitura;
+	}
+
+	public Short getComLeitura() {
+		return comLeitura;
+	}
+
+	public void setComLeitura(Short comLeitura) {
+		this.comLeitura = comLeitura;
+	}
+
+	public Short getIndicadorUso() {
+		return indicadorUso;
+	}
+
+	public void setIndicadorUso(Short indicadorUso) {
+		this.indicadorUso = indicadorUso;
+	}
+
+	public Date getUltimaAlteracao() {
+		return ultimaAlteracao;
+	}
+
+	public void setUltimaAlteracao(Date ultimaAlteracao) {
+		this.ultimaAlteracao = ultimaAlteracao;
+	}
 }
