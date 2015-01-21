e182b3c1cac9ea2671ef3753b30e4c952db610cf
Pamela Gatinho <pamelagatinho@gmail.com>
2015-01-21 15:41:22 -0300

    Correção

--- ambiente-test/scripts/bootstrap.sql
+++ ambiente-test/scripts/bootstrap.sql
@@ -7,7 +7,6 @@
 -- Started on 2015-01-14 15:29:25 BRT
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
