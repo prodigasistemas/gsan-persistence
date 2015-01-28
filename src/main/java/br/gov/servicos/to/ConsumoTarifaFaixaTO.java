1f335fd16cb317ab62dd74db8a78212e84ec8c9c
Felipe Santos <felipesantos2089@gmail.com>
2015-01-28 10:45:09 -0300

    Implementação e testes para o repositório de ConsumoTarifaFaixa da Linha 10

---
+++ src/main/java/br/gov/servicos/to/ConsumoTarifaFaixaTO.java
@@ -0,0 +1,100 @@
+package br.gov.servicos.to;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ConsumoTarifaFaixaTO implements Serializable {
+
+	private static final long serialVersionUID = 1663802583652640406L;
+
+	private Integer idConsumoTarifa;
+	
+	private Date dataVigencia;
+	
+	private Integer idCategoria;
+	
+	private Integer idSubcategoria;
+	
+	private Integer numeroConsumoFaixaInicio;
+
+    private Integer numeroConsumoFaixaFim;
+
+    private BigDecimal valorConsumoTarifa;
+
+    public ConsumoTarifaFaixaTO() {}
+    
+	public ConsumoTarifaFaixaTO(
+			Integer idConsumoTarifa,
+			Date dataVigencia,
+			Integer idCategoria,
+			Integer idSubcategoria,
+			Integer numeroConsumoFaixaInicio,
+			Integer numeroConsumoFaixaFim,
+			BigDecimal valorConsumoTarifa) {
+		super();
+		this.idConsumoTarifa = idConsumoTarifa;
+		this.dataVigencia = dataVigencia;
+		this.idCategoria = idCategoria;
+		this.idSubcategoria = idSubcategoria;
+		this.numeroConsumoFaixaInicio = numeroConsumoFaixaInicio;
+		this.numeroConsumoFaixaFim = numeroConsumoFaixaFim;
+		this.valorConsumoTarifa = valorConsumoTarifa;
+	}
+
+	public Integer getIdConsumoTarifa() {
+		return idConsumoTarifa;
+	}
+
+	public void setIdConsumoTarifa(Integer idConsumoTarifa) {
+		this.idConsumoTarifa = idConsumoTarifa;
+	}
+
+	public Date getDataVigencia() {
+		return dataVigencia;
+	}
+
+	public void setDataVigencia(Date dataVigencia) {
+		this.dataVigencia = dataVigencia;
+	}
+
+	public Integer getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(Integer idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public Integer getIdSubcategoria() {
+		return idSubcategoria;
+	}
+
+	public void setIdSubcategoria(Integer idSubcategoria) {
+		this.idSubcategoria = idSubcategoria;
+	}
+
+	public Integer getNumeroConsumoFaixaInicio() {
+		return numeroConsumoFaixaInicio;
+	}
+
+	public void setNumeroConsumoFaixaInicio(Integer numeroConsumoFaixaInicio) {
+		this.numeroConsumoFaixaInicio = numeroConsumoFaixaInicio;
+	}
+
+	public Integer getNumeroConsumoFaixaFim() {
+		return numeroConsumoFaixaFim;
+	}
+
+	public void setNumeroConsumoFaixaFim(Integer numeroConsumoFaixaFim) {
+		this.numeroConsumoFaixaFim = numeroConsumoFaixaFim;
+	}
+
+	public BigDecimal getValorConsumoTarifa() {
+		return valorConsumoTarifa;
+	}
+
+	public void setValorConsumoTarifa(BigDecimal valorConsumoTarifa) {
+		this.valorConsumoTarifa = valorConsumoTarifa;
+	}
+}
---
+++ src/test/java/br/gov/servicos/faturamento/ConsumoTarifaFaixaRepositorioTest.java
@@ -0,0 +1,58 @@
+package br.gov.servicos.faturamento;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.persistence.UsingDataSet;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.gov.persistence.util.SingleDeployment;
+import br.gov.servicos.to.ConsumoTarifaFaixaTO;
+
+@RunWith(Arquillian.class)
+public class ConsumoTarifaFaixaRepositorioTest extends SingleDeployment {
+
+	@Inject
+	private ConsumoTarifaFaixaRepositorio repositorio;
+	
+	@Test
+	@UsingDataSet("consumo_tarifa_faixa.yml")
+	public void dadosConsumoTarifaFaixa() {
+		List<Integer> ids = new ArrayList<Integer>();
+		ids.add(1);
+		ids.add(3);
+		
+		List<ConsumoTarifaFaixaTO> lista = repositorio.dadosConsumoTarifaFaixa(ids);
+		
+		ConsumoTarifaFaixaTO to = lista.get(0);
+		assertEquals(1, to.getIdConsumoTarifa().intValue());
+		assertEquals("2014-09-01", to.getDataVigencia().toString());
+		assertEquals(0, to.getNumeroConsumoFaixaInicio().intValue());
+		assertEquals(10, to.getNumeroConsumoFaixaFim().intValue());
+		assertEquals(new BigDecimal("14.00"), to.getValorConsumoTarifa());
+		
+		to = lista.get(1);
+		assertEquals(3, to.getIdConsumoTarifa().intValue());
+		assertEquals("2014-02-20", to.getDataVigencia().toString());
+		assertEquals(21, to.getNumeroConsumoFaixaInicio().intValue());
+		assertEquals(30, to.getNumeroConsumoFaixaFim().intValue());
+		assertEquals(new BigDecimal("100.00"), to.getValorConsumoTarifa());
+	}
+	
+	@Test
+	@UsingDataSet("consumo_tarifa_faixa.yml")
+	public void dadosConsumoTarifaFaixaVazio() {
+		List<Integer> ids = new ArrayList<Integer>();
+		ids.add(2);
+		
+		assertTrue(repositorio.dadosConsumoTarifaFaixa(ids).isEmpty());
+	}
+}
