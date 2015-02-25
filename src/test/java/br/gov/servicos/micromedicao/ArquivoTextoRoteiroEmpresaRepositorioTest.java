159df7585898498e73eff948305c915895c48ae6
Felipe <felipesantos2089@gmail.com>
2015-02-25 18:14:08 -0300

    Implementação e teste para pesquisarPorGrupoEReferencia na classe ArquivoTextoRoteiroEmpresaRepositorio

--- src/test/java/br/gov/servicos/micromedicao/ArquivoTextoRoteiroEmpresaRepositorioTest.java
+++ src/test/java/br/gov/servicos/micromedicao/ArquivoTextoRoteiroEmpresaRepositorioTest.java
@@ -2,6 +2,8 @@ package br.gov.servicos.micromedicao;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.jboss.arquillian.junit.Arquillian;
@@ -21,7 +23,7 @@ public class ArquivoTextoRoteiroEmpresaRepositorioTest extends SingleDeployment 
 	
 	@Test
 	@UsingDataSet("arquivo_texto_roteiro_empresa.yml")
-	public void recuperarArquivoTextoRoteiroEmpresa() {
+	public void pesquisarPorRotaEReferencia() {
 		ArquivoTextoRoteiroEmpresa arquivo = repositorio.pesquisarPorRotaEReferencia(1, 201502);
 
 		assertEquals(3, arquivo.getId().intValue());
@@ -34,5 +36,16 @@ public class ArquivoTextoRoteiroEmpresaRepositorioTest extends SingleDeployment 
 	public void deletarArquivo() {
 		repositorio.excluir(3);
 		repositorio.excluir(4);
+	}
+	
+	@Test
+	@UsingDataSet("arquivo_texto_roteiro_empresa.yml")
+	public void pesquisarPorGrupoEReferencia() {
+		List<ArquivoTextoRoteiroEmpresa> arquivos = repositorio.pesquisarPorGrupoEReferencia(1, 201502);
+
+		ArquivoTextoRoteiroEmpresa arquivo = arquivos.get(0);
+		
+		assertEquals(3, arquivo.getId().intValue());
+		assertEquals(2, arquivo.getSituacaoTransmissaoLeitura().intValue());
 	}
 }
