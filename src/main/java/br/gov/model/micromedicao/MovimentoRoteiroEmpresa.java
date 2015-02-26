8918925ce189e1abae4c2c61f64793b87a1ecede
Felipe <felipesantos2089@gmail.com>
2015-02-26 18:06:03 -0300

    Gerar Arquivo Texto para Microcoletor

--- src/main/java/br/gov/model/micromedicao/MovimentoRoteiroEmpresa.java
+++ src/main/java/br/gov/model/micromedicao/MovimentoRoteiroEmpresa.java
@@ -25,6 +25,7 @@ import br.gov.model.cadastro.Imovel;
 import br.gov.model.cadastro.ImovelPerfil;
 import br.gov.model.cadastro.Localidade;
 import br.gov.model.cadastro.Logradouro;
+import br.gov.model.cadastro.SetorComercial;
 import br.gov.model.faturamento.FaturamentoGrupo;
 
 @Entity
@@ -160,6 +161,12 @@ public class MovimentoRoteiroEmpresa implements Serializable {
 	
 	@Column(name = "mrem_nnloteimovel")
 	private String numeroLoteImovel;
+	
+	@Column(name = "mrem_nnsubloteimovel")
+	private String numeroSubloteImovel;
+	
+	@Column(name = "mrem_dsabrevcatgimovel")
+	private String descricaoAbreviadaCategoriaImovel;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "ftgr_id")
@@ -188,6 +195,10 @@ public class MovimentoRoteiroEmpresa implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "loca_id")
 	private Localidade localidade;
+	
+	@ManyToOne
+	@JoinColumn(name = "stcm_id")
+	private SetorComercial setorComercial;
 
 	@ManyToOne
 	@JoinColumn(name = "imov_id")
@@ -599,6 +610,46 @@ public class MovimentoRoteiroEmpresa implements Serializable {
 
 	public void setLocalidade(Localidade localidade) {
 		this.localidade = localidade;
+	}
+
+	public Integer getNumeroQuadra() {
+		return numeroQuadra;
+	}
+
+	public void setNumeroQuadra(Integer numeroQuadra) {
+		this.numeroQuadra = numeroQuadra;
+	}
+
+	public String getNumeroLoteImovel() {
+		return numeroLoteImovel;
+	}
+
+	public void setNumeroLoteImovel(String numeroLoteImovel) {
+		this.numeroLoteImovel = numeroLoteImovel;
+	}
+
+	public String getNumeroSubloteImovel() {
+		return numeroSubloteImovel;
+	}
+
+	public void setNumeroSubloteImovel(String numeroSubloteImovel) {
+		this.numeroSubloteImovel = numeroSubloteImovel;
+	}
+
+	public String getDescricaoAbreviadaCategoriaImovel() {
+		return descricaoAbreviadaCategoriaImovel;
+	}
+
+	public void setDescricaoAbreviadaCategoriaImovel(String descricaoAbreviadaCategoriaImovel) {
+		this.descricaoAbreviadaCategoriaImovel = descricaoAbreviadaCategoriaImovel;
+	}
+
+	public SetorComercial getSetorComercial() {
+		return setorComercial;
+	}
+
+	public void setSetorComercial(SetorComercial setorComercial) {
+		this.setorComercial = setorComercial;
 	}
 
 	public Imovel getImovel() {
--- src/main/java/br/gov/model/util/IOUtil.java
+++ src/main/java/br/gov/model/util/IOUtil.java
@@ -1,17 +1,26 @@
 package br.gov.model.util;
 
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.util.zip.GZIPOutputStream;
 
 public class IOUtil {
 
+	public static void criarArquivo(String nome, String caminho, String conteudo) {
+		File arquivo = new File(caminho + nome);
+		IOUtil.escreverArquivo(arquivo, conteudo);
+		IOUtil.comprimirParaGzip(arquivo);
+		arquivo.delete();
+	}
+	
 	public static void escreverArquivo(File arquivo, String texto) {
 		try {
 			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(arquivo)));
@@ -44,5 +53,24 @@ public class IOUtil {
 		}
 
 		return comprimido;
+	}
+
+	public static byte[] transformarObjetoParaBytes(Object objeto) {
+		byte[] retorno = null;
+
+		try {
+			ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+			ObjectOutputStream output = new ObjectOutputStream(byteOutput);
+			output.writeObject(objeto);
+
+			retorno = byteOutput.toByteArray();
+
+			output.close();
+			byteOutput.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return retorno;
 	}
 }
