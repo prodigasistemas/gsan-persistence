61176865ea026fa9333b5e954fcc97fca1375dc1
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2015-04-17 17:52:03 -0300

    prodigasistemas/gsan-batch#19 - Metodo para excluir as ocorrencias de atividades de processo

--- src/main/java/br/gov/servicos/batch/ControleProcessoAtividadeRepositorio.java
+++ src/main/java/br/gov/servicos/batch/ControleProcessoAtividadeRepositorio.java
@@ -7,4 +7,14 @@ import br.gov.model.util.GenericRepository;
 
 @Stateless
 public class ControleProcessoAtividadeRepositorio extends GenericRepository<Integer, ControleProcessoAtividade>{
+    
+    public void apagarAtividadesDeProcesso(Integer idProcessoIniciado){
+        StringBuilder sql = new StringBuilder();
+        sql.append("delete ControleProcessoAtividade as controle")
+        .append(" where controle.processoIniciado.id = :id");
+        
+        entity.createQuery(sql.toString())
+        .setParameter("id", idProcessoIniciado)
+        .executeUpdate();
+    }
 }
