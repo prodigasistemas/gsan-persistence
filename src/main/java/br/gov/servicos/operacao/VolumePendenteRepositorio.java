11d136ff106025b3632ad7ea39e706d2ba2a573f
rcasseb <rcasseb@gmail.com>
2015-06-01 16:06:09 -0300

    refactoring

--- src/main/java/br/gov/servicos/operacao/VolumePendenteRepositorio.java
+++
@@ -1,9 +0,0 @@
-package br.gov.servicos.operacao;
-
-import br.gov.model.operacao.LancamentoPendente;
-import br.gov.model.util.GenericRepository;
-
-public class VolumePendenteRepositorio extends GenericRepository<Integer, LancamentoPendente>{
-
-	
-}
