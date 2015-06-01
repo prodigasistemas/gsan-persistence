e89680702e6d8d30c815de7b26802175febece1d
rcasseb <rcasseb@gmail.com>
2015-06-01 16:00:46 -0300

    fix lista de pendencias nas medicoes de volume de agua

---
+++ src/main/java/br/gov/servicos/operacao/VolumePendenteRepositorio.java
@@ -0,0 +1,9 @@
+package br.gov.servicos.operacao;
+
+import br.gov.model.operacao.LancamentoPendente;
+import br.gov.model.util.GenericRepository;
+
+public class VolumePendenteRepositorio extends GenericRepository<Integer, LancamentoPendente>{
+
+	
+}
