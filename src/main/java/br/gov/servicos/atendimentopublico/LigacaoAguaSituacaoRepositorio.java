5987c67abc7a1e95bcca69e3b2ef048b8518b68d
Paulo Igor <piagodinho@gmail.com>
2016-11-09 10:55:52 -0300

    union nao e suportado pelo HQL

--- src/main/java/br/gov/servicos/atendimentopublico/LigacaoAguaSituacaoRepositorio.java
+++ src/main/java/br/gov/servicos/atendimentopublico/LigacaoAguaSituacaoRepositorio.java
@@ -16,28 +16,41 @@ public class LigacaoAguaSituacaoRepositorio extends GenericRepository<Integer, L
 	
 	public LigacaoAguaSituacao buscarLigacaoAguaSituacao(Integer imovelId, Integer anoMesReferencia) {
 		StringBuilder sql = new StringBuilder();
-		
-		sql.append("SELECT ligacaoAguaSituacao ")
-		   .append("FROM Conta conta ")
-		   .append("INNER JOIN LigacaoAguaSituacao ligacaoAguaSituacao ")
-		   .append("WHERE imovel.id = :idImovel ")
-		   .append("AND conta.referenciaContabil = :anoMesReferencia ")
-		   .append("UNION ")
-		   .append("SELECT ligacaoAguaSituacao ")
-		   .append("FROM ContaHistorico contaHistorico ")
-		   .append("INNER JOIN LigacaoAguaSituacao ligacaoAguaSituacao ")
-		   .append("WHERE imovel.id = :idImovel ")
-		   .append("AND conta.referenciaContabil = :anoMesReferencia ");
+
+		LigacaoAguaSituacao situacaoConta = null;
 		
 		try {
-			return entity.createQuery(sql.toString(), LigacaoAguaSituacao.class)
-					.setParameter("idImovel", imovelId)
-					.setParameter("anoMesReferencia", anoMesReferencia)
-					.setMaxResults(1)
-					.getSingleResult();
-			
+			sql.append("SELECT ligacaoAguaSituacao ")
+				.append("FROM Conta conta ")
+				.append("INNER JOIN LigacaoAguaSituacao ligacaoAguaSituacao ")
+				.append("WHERE imovel.id = :idImovel ")
+				.append("AND conta.referenciaContabil = :anoMesReferencia ");
+		   
+			situacaoConta = entity.createQuery(sql.toString(), LigacaoAguaSituacao.class)
+									.setParameter("idImovel", imovelId)
+									.setParameter("anoMesReferencia", anoMesReferencia)
+									.setMaxResults(1)
+									.getSingleResult();
+
 		} catch (NoResultException e) {
-			return null;
+			try{
+				sql.append("SELECT ligacaoAguaSituacao ")
+				.append("FROM ContaHistorico contaHistorico ")
+				.append("INNER JOIN LigacaoAguaSituacao ligacaoAguaSituacao ")
+				.append("WHERE imovel.id = :idImovel ")
+				.append("AND conta.referenciaContabil = :anoMesReferencia ");
+				
+				situacaoConta = entity.createQuery(sql.toString(), LigacaoAguaSituacao.class)
+						.setParameter("idImovel", imovelId)
+						.setParameter("anoMesReferencia", anoMesReferencia)
+						.setMaxResults(1)
+						.getSingleResult();
+				
+			} catch (NoResultException e2) {
+				return null;
+			}
 		}
+		
+		return situacaoConta;
 	}
 }
