b60d375e99d5dea47cbba5711ba394c68ad5754b
Paulo Igor <piagodinho@gmail.com>
2016-11-08 23:21:36 -0300

    correcao do parametro da consulta

--- src/main/java/br/gov/servicos/desempenho/ContratoMedicaoRepositorio.java
+++ src/main/java/br/gov/servicos/desempenho/ContratoMedicaoRepositorio.java
@@ -22,7 +22,7 @@ public class ContratoMedicaoRepositorio {
 		
 		StringBuilder sql = new StringBuilder();
 			sql.append("SELECT contrato from ContratoMedicao contrato ")
-				.append(" WHERE contrato.vigenciaFinal <= :dataAtual");
+				.append(" WHERE contrato.vigenciaFinal <= :dataReferencia");
 			
 			return entity.createQuery(sql.toString(), ContratoMedicao.class)
 					.setParameter("dataReferencia", dataFimReferencia)
